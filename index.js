b095c4da210cb3573da534e8e7493605f82c236d
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-05-21 13:23:12 -0600

    Install nodemon

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/')
+app.get('/', (req, res) => {
+  res.send('Hello World from Node js')
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Node API listening on port ${port}`)
+})
